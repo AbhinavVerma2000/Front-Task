0278167e81751c1f2921a8210da79bb794466170
Abhinav Verma <>
2025-02-06 17:07:17 +0530

    Create index.js

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Inventory from "./Inventory/index";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Button, message } from "antd";
+
+const Profile = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        message.success("Logout Successful");
+        navigate("/login");
+      })
+      .catch((error) => {
+        message.error(error.message);
+      });
+  };
+  if (!localStorage.getItem("user")) {
+    navigate("/login");
+  }
+  return (
+    <div>
+      <div className="w-screen text-right"><Button type="primary" danger onClick={() => logout()}>Logout</Button></div>
+      
+      <Inventory />
+    </div>
+  );
+};
+
+export default Profile;
